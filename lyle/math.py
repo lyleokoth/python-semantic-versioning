e2fd156894fabf026ba449c2e316d43b4bac31ff
lyleokoth <okothlyleochieng@gmail.com>
2022-03-29 09:34:51 +0300

    feat: added new subtract functionality

--- lyle/math.py
+++ lyle/math.py
@@ -9,3 +9,15 @@ def add(x: int, y: int) -> int:
                     int: sum of x and y
     """
     return x + y
+
+def subtract(x: int, y: int) -> int:
+    """subtract one number from another
+
+    Args:
+                    x (int): first number to subtract
+                    y (int): second number to subtract
+
+    Returns:
+                    int: resut of x subtract y
+    """
+    return x - y
